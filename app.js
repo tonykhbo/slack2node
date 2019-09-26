4330b84291aa9eaa93e0aaa03494fe6513d0f462
Andres Romero <aromero@redhat.com>
2019-09-26 16:51:06 -0400

    Fixing double postings

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const { WebClient } = require('@slack/web-api');
 
 // An access token (from your Slack app or custom integration - usually xoxb)
 const slack_token = process.env.SLACK_TOKEN;
+  // Sending a message requires a channel ID, a DM ID, an MPDM ID, or a group ID
+  // The following value is used as an example
+const whiteChannelId = process.env.WHITE_CHANNEL_ID;
+const redChannelId = process.env.RED_CHANNEL_ID;
 
 // The client is initialized and then started to get an active connection to the platform
 const rtm = new RTMClient(slack_token);
@@ -21,23 +25,28 @@ rtm.start()
 // Calling `rtm.on(eventName, eventHandler)` allows you to handle events (see: https://api.slack.com/events)
 // When the connection is active, the 'ready' event will be triggered
 rtm.on('ready', async () => {
-
-  // Sending a message requires a channel ID, a DM ID, an MPDM ID, or a group ID
-  // The following value is used as an example
-  const channelId = process.env.CHANNEL_ID;
-
   // The RTM client can send simple string messages
-  const res = await rtm.sendMessage('Node Server Up', channelId);
+  const whiteRes = await rtm.sendMessage('Node Server Up', whiteChannelId);
+  const redRes = await rtm.sendMessage('Node Server Up', redChannelId);
 
   // `res` contains information about the sent message
-  console.log('Message sent: ', res.ts);
+  console.log('Message sent: ', whiteRes.ts);
+  console.log('Message sent: ', redRes.ts);
 });
 
 // After the connection is open, your app will start receiving other events.
 rtm.on('message', (event) => {
+  var color;
+  console.log(event);
+  if ( event.channel == whiteChannelId ) {
+    color = 'white';
+  } else if ( event.channel == redChannelId ) {
+    color = 'red';
+  } else {
+    return;
+  }
   // The argument is the event as shown in the reference docs.
   // For example, https://api.slack.com/events/user_typing
-  console.log(event);
   (async () => {
   // See: https://api.slack.com/methods/chat.postMessage
     const res = await web.users.info({token: slack_token, user: event.user});
@@ -45,7 +54,7 @@ rtm.on('message', (event) => {
     if ((event.text.toLowerCase() == 'left' ) || (event.text.toLowerCase() == 'right' ) || (event.text.toLowerCase() == 'up' ) || (event.text.toLowerCase() == 'down' )) {
       console.log(event.text);
       request.post(
-        `https://directive-producer-demojam-zombie.apps.akrohg-openshift.redhatgov.io/camel/rest/produce/${process.env.COLOR}`,
+        `https://directive-producer-demojam-zombie.apps.akrohg-openshift.redhatgov.io/camel/rest/produce/${color}`,
         { json: { username: res.user.name, direction: event.text  } },
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
